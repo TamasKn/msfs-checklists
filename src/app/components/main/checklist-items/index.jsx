f0b50ff3535c46112ac775cc3ae0b65cc13d6c03
Tamas Knisz <tamas.knisz@gmail.com>
2025-10-07 17:11:15 +0200

    checklist comp update

--- src/app/components/main/checklist-items/index.jsx
+++ src/app/components/main/checklist-items/index.jsx
@@ -29,9 +29,9 @@ export default function ChecklistItems() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8">
-      <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 dark:text-white mb-6">
-        {cessnaLongitudeChecklist.name} Checklist
+    <div className="max-w-4xl mx-auto p-4 mt-12 sm:p-6 lg:p-8">
+      <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 dark:text-white mb-6 text-left">
+        {cessnaLongitudeChecklist.name}
       </h1>
       <div className="space-y-2">
         {cessnaLongitudeChecklist.checklist.map((section, index) => {
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import ChecklistItems from "./components/main/checklist-items";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="text-center p-4 sm:p-6 lg:p-8">
+      <h1 className="text-2xl sm:text-4xl font-bold text-gray-900 dark:text-white mb-6">
       MSFS Checklists
+      </h1>
 
       <ChecklistItems />
     </div>
