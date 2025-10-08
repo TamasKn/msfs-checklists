f131671b04d1758f2429dd90bc73aa33c4b7427b
Tamas Knisz <tamas.knisz@gmail.com>
2025-10-08 14:56:05 +0200

    Longitude specs update

--- src/data/cessna-172/specs.js
+++ src/data/cessna-172/specs.js
@@ -6,16 +6,16 @@ export const cessna172Specs = {
     {
       title: 'Limits & Specs',
       items: [
-        { 'Cruise Speed': '124 KTAS' },
+        { 'Cruise Speed': '124 KIAS' },
         { Range: '640 NM' },
         { 'Maximum Operating Altitude': '14,000 ft' },
         { 'Rate of Climb': '730 ft/min' },
         { VMO: '0 KTAS' },
         { MMO: '0' },
-        { VLE: '0 KTAS' },
-        { 'VFE (Flaps °)': '0 KTAS' },
-        { 'VFE (Flaps °)': '0 KTAS' },
-        { 'VFE (Flaps Full)': '0 KTAS' },
+        { VLE: '0 KIAS' },
+        { 'VFE (Flaps °)': '0 KIAS' },
+        { 'VFE (Flaps °)': '0 KIAS' },
+        { 'VFE (Flaps Full)': '0 KIAS' },
         { Payload: '2,550 lb' },
         { 'Fuel Capacity': '53 gal (usable)' },
         { 'Stall Speed': '48 KCAS' }
--- src/data/cessna-longitude/specs.js
+++ src/data/cessna-longitude/specs.js
@@ -10,25 +10,43 @@ export const cessnaLongitudeSpecs = {
           'Maximum Operating Altitude': '45,000 ft'
         },
         {
-          'Cruise Speed': '320 KTAS'
+          'Maximum Takeoff and Landing Altitude Limit': '14,000 ft'
+        },
+        {
+          'Cruise Speed': '320 KIAS'
         },
         {
           Range: '3,500 NM'
         },
         { VMO: '483 KTAS' },
         { MMO: '0.724' },
-        { VLE: '230 KTAS' },
-        { 'VFE (Flaps 1)': '250 KTAS' },
-        { 'VFE (Flaps 2)': '230 KTAS' },
-        { 'VFE (Flaps Full)': '180 KTAS' },
+        { VLE: '230 KIAS' },
+        { 'VFE (Flaps 1)': '250 KIAS' },
+        { 'VFE (Flaps 2)': '230 KIAS' },
+        { 'VFE (Flaps Full)': '180 KIAS' },
         {
-          Payload: '4000 lb / 1814 kg'
+          'Maximum Landing Sink Rate': '600 ft/min'
         },
         {
-          'Fuel Capacity': '14,593 lb / 6619 kg'
+          'Maximum Tire Groundspeed': '195 KNT'
+        },
+        {
+          Payload: '4,000 lb | 1814 kg'
+        },
+        {
+          'Maximum Takeoff Weight': '39,500 lb | 17915 kg'
+        },
+        {
+          'Maximum Landing Weight': '33,500 lb | 15195 kg'
+        },
+        {
+          'Fuel Capacity': '14,593 lb | 6619 kg'
         },
         {
           'Fuel Consumption': '816.50 L/hr'
+        },
+        {
+          'Stall Speed': '85-120 KIAS'
         }
       ]
     }
