e7943bcc6d819cc20625cafb1cebde8d21ac333e
Tamas Knisz <tamas.knisz@gmail.com>
2025-10-09 11:37:26 +0200

    incorrect viewport removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,20 @@
-import { Geist_Mono, Funnel_Sans } from "next/font/google";
-import "./globals.css";
+import { Geist_Mono, Funnel_Sans } from 'next/font/google'
+import './globals.css'
 
 const funnelSans = Funnel_Sans({
-  variable: "--font-funnel-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-funnel-sans',
+  subsets: ['latin']
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
 
 export const metadata = {
-  title: "MSFS Checklists",
-  description: "Checklists for Microsoft Flight Simulator",
-  viewport: "width=device-width, initial-scale=1",
-};
+  title: 'MSFS Checklists',
+  description: 'Checklists for Microsoft Flight Simulator'
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -23,10 +22,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${funnelSans.variable} ${geistMono.variable} antialiased min-h-screen w-full`}
       >
-        <div className="w-full min-h-screen flex flex-col">
-          {children}
-        </div>
+        <div className="w-full min-h-screen flex flex-col">{children}</div>
       </body>
     </html>
-  );
+  )
 }
