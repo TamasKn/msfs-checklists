afb395900be61c4201b0e82d4258281f9c1ff749
Tamas Knisz <tamas.knisz@gmail.com>
2025-10-23 11:51:27 +0200

    CJ4 career figures update

--- src/data/cj4/career.js
+++ src/data/cj4/career.js
@@ -1,64 +1,64 @@
 export const CJ4Career = {
   id: 'C25C',
   costs: {
-    buyPriceBase: 28500000, // base price for a new aircraft
-    sellPriceBase: 17000000, // base price for a used aircraft
-    leasePriceBase: 3750, // per flight hour
-    insuranceBase: 550, // per flight hour
+    buyPriceBase: 9000000, // base price for a new aircraft
+    sellPriceBase: 6000000, // base price for a used aircraft
+    leasePriceBase: 2500, // per flight hour
+    insuranceBase: 425, // per flight hour
     maintenance: {
-      base: 1700, // per flight hour
+      base: 1200, // per flight hour
       issues: {
         // possible issues, cost and chance of occurring
         engine: {
-          base: 21000,
+          base: 15000,
           chance: 0.008
         },
         landingGear: {
-          base: 5700,
+          base: 4000,
           chance: 0.23
         },
         fuselage: {
-          base: 3900,
+          base: 2800,
           chance: 0.33
         },
         wing: {
-          base: 4800,
+          base: 3500,
           chance: 0.26
         },
         empennage: {
-          base: 2800,
+          base: 2000,
           chance: 0.29
         },
         electrical: {
-          base: 1950,
+          base: 1500,
           chance: 0.35
         },
         hydraulic: {
-          base: 1750,
+          base: 1300,
           chance: 0.3
         },
         pressurization: {
-          base: 2250,
+          base: 1800,
           chance: 0.28
         },
         avionics: {
-          base: 4200,
+          base: 3000,
           chance: 0.24
         },
         interior: {
-          base: 1800,
+          base: 1200,
           chance: 0.38
         },
         lights: {
-          base: 920,
+          base: 700,
           chance: 0.4
         },
         navigation: {
-          base: 2350,
+          base: 1800,
           chance: 0.26
         },
         communication: {
-          base: 1850,
+          base: 1400,
           chance: 0.36
         }
       }
@@ -68,17 +68,17 @@ export const CJ4Career = {
     storageBase: 100 // per day
   },
   reward: {
-    base: 8200, // base reward for a flight
-    perDistance: 15.55, // per nautical mile
+    base: 5300, // base reward for a flight
+    perDistance: 12.5, // per nautical mile
     bonus: {
       min: 2000, // minimum total
-      max: 7200, // maximum total
-      xpMultiplier: 1.027 // multiplier for XP
+      max: 5500, // maximum total
+      xpMultiplier: 1.018 // multiplier for XP
     }
   },
   xp: {
-    base: 15.75, // base XP for a flight
-    perDistance: 0.416115, // per nautical mile
-    perFlightHour: 9.8 // per flight hour
+    base: 12.45, // base XP for a flight
+    perDistance: 0.35, // per nautical mile
+    perFlightHour: 7.2 // per flight hour
   }
 }
