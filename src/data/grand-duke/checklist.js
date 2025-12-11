716ce6949bbd98c85d3a63820239d8b11341390f
Tamas Knisz <tamas.knisz@gmail.com>
2025-12-11 11:42:54 +0100

    turbine duke checklist and procedure update

--- src/data/grand-duke/checklist.js
+++ src/data/grand-duke/checklist.js
@@ -12,12 +12,14 @@ export const grandDukeChecklist = {
         { 'Power Levers': 'IDLE' },
         { 'Prop Levers': 'HIGH RPM' },
         { 'Condition Levers': 'CUTOFF' },
-        { 'Fuel Selectors': 'ON' }
+        { 'Fuel Selectors': 'ON' },
+        { 'VFR/IFR CLEARANCE': 'OBTAINED' }
       ]
     },
     {
       title: 'Before Engine Start',
       items: [
+        { 'START CLEARANCE': 'OBTAINED' },
         { 'Beacon': 'ON' },
         { 'Battery Master': 'ON' },
         { 'Fuel Quantity': 'CHECK' },
@@ -35,32 +37,44 @@ export const grandDukeChecklist = {
     {
       title: 'Engine Start',
       items: [
-        { 'Right Starter': 'ON' },
+        { 'R Starter': 'ON' },
         { 'Bus Volts': 'MIN 15V' },
-        { 'Ng': 'MIN 15%' },
-        { 'Right Condition Lever': 'RUN' },
+        { 'NG%': 'MIN 15%' },
+        { 'R Condition Lever': 'RUN' },
         { 'ITT': '< 1090°C' },
-        { 'Right Starter': 'OFF AT 52% NG' },
-        { 'Right Ignition': 'OFF' },
+        { 'R Starter': 'OFF AT 52% NG' },
+        { 'R Ignition': 'OFF' },
         { 'Engine Instruments': 'CHECK' },
-        { 'Right Generator': 'ON' },
-        { 'Right Generator Load': '< 60' },
+        { 'R Generator': 'ON' },
+        { 'R Generator Load': '< 60' },
         { 'Bus Volts': 'MIN 28V' },
         { 'Battery Temp': '< 48°C' },
-        { 'Left Engine': 'REPEAT' }
+        { 'L Engine': 'REPEAT' }
       ]
     },
     {
       title: 'After Engine Start',
-      items: [{ 'Inverter': 'MAIN' }]
+      items: [{ 'Inverter': 'MAIN' }, { 'Taxi Lights': 'ON' }]
     },
     {
       title: 'Takeoff',
-      items: [{ 'ITT': '< 805°C' }]
+      items: [
+        { 'Runway/Departure Clearance': 'OBTAINED' },
+        { 'Taxi Lights': 'OFF' },
+        { 'Landing Lights': 'ON' },
+        { 'ITT': '< 805°C' },
+        { 'Runway Alignment': 'CONFIRM' },
+        { 'Heading Bug': 'SET' },
+        { 'Throttles': 'TOGA' },
+        { 'Positive Rate': 'GEAR UP' },
+        { 'AP & AT': 'ENGAGE, AS REQ' },
+        { 'Flaps': 'RETRACT at Safe Speed' }
+      ]
     },
     {
       title: 'Climb',
       items: [
+        { 'Landing Lights': 'OFF' },
         { 'Ignition': 'AUTO/OFF' },
         { 'Oil Doors': 'OPEN' },
         { 'Props': '2190 RPM' }
@@ -72,14 +86,29 @@ export const grandDukeChecklist = {
         { 'Recognition Lights': 'OFF' },
         { 'Oil Doors': 'CLOSED' },
         { 'Fuel Imbalance': 'MAX 25 GAL' },
-        { 'Props': '2000 RPM' }
+        { 'Props': '2000 RPM' },
+        {
+          'BARO': 'Set to STD (if applicable)',
+          'info': 'STD: 29.92 in / 1013 hPa'
+        },
+        {
+          'TOD calculation': 'Obtained',
+          'info':
+            'Distance (NM): (Altitude to lose in ft / 1000) * 3, VS (fpm): 5 x Ground Speed (kts)'
+        }
       ]
     },
     {
       title: 'Descent/Approach',
       items: [
-        { 'Fuel Selectors': 'ON' },
+        { 'Clearance': 'OBTAINED' },
+        { 'TOD': 'Start descent as calculated' },
         { 'Recognition Lights': 'ON' },
+        { 'Landing Lights': 'ON' },
+        { 'ATIS & Weather': 'CHECK' },
+        { 'Power': 'Reduce for descent' },
+        { 'Propeller': 'As required' },
+        { 'Fuel Selectors': 'ON' },
         { 'Windshield Heat': 'OFF' },
         { 'Prop Heat': 'OFF' },
         { 'Ignition': 'AUTO' },
@@ -92,7 +121,13 @@ export const grandDukeChecklist = {
     },
     {
       title: 'After Landing',
-      items: [{ 'Oil Doors': 'CLOSED' }, { 'Pitot Heat': 'OFF' }]
+      items: [
+        { 'Taxi & Parking Clearance': 'OBTAINED' },
+        { 'Oil Doors': 'CLOSED' },
+        { 'Pitot Heat': 'OFF' },
+        { 'Taxi Lights': 'ON' },
+        { 'Landing Lights': 'OFF' }
+      ]
     },
     {
       title: 'Shutdown',
