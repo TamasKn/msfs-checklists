72c368359899bdca79e995bb6d24736004b89c37
Tamas Knisz <tamas.knisz@gmail.com>
2026-04-29 10:45:21 +0200

    Baron 58P specs update

--- src/data/aircrafts/baron-58p/specs.js
+++ src/data/aircrafts/baron-58p/specs.js
@@ -7,16 +7,15 @@ export const baron58PSpecs = {
       title: 'Limits & Specs',
       items: [
         { 'Max Operating Altitude': '25,000 ft' },
-        { 'Cruise Speed': '228 KTAS' },
-        { 'Range': '1,229 NM' },
-        { 'Rate of Climb': '1,700 ft/min' },
-        { 'VNE': '223 KIAS' },
-        { 'VFE': '152 KIAS' },
-        { 'VLE': '152 KIAS' },
-        { 'VLO': '122 KIAS' },
-        { 'Fuel Consumption (G/hr)': 34 },
-        { 'Payload': '700 lb' },
-        { 'Fuel Capacity': '194 gal' },
+        { 'Cruise Speed': '245 KTAS' },
+        { 'Range': '1,186 NM' },
+        { 'Rate of Climb': '1,850 ft/min' },
+        { 'VNE': '235 KIAS' },
+        { 'VFE': '143 KIAS' },
+        { 'VLE': '177 KIAS' },
+        { 'VMO': '261 KTAS' },
+        { 'Fuel Consumption (L/hr)': 139 },
+        { 'Payload': '2,230 lb | 1,012 kg' },
         { 'Stall Speed': '84 KCAS' }
       ]
     }
