dc0d6dd3d8d9e5f245ebfa59443174956f06613e
Tamas Knisz <tamas.knisz@gmail.com>
2026-05-04 14:44:00 +0200

    A350 checklist update

--- src/data/aircrafts/airbus-a350/checklist.js
+++ src/data/aircrafts/airbus-a350/checklist.js
@@ -4,140 +4,341 @@ export const airbusA350Checklist = {
   name: AircraftName.AirbusA350,
   checklist: [
     {
+      title: 'MISC',
+      items: [
+        { 'Flight Plan': 'Imported' },
+        { 'Departure, Arrival, Approach': 'SET' }
+      ]
+    },
+    {
+      title: 'Cockpit Preparation',
+      items: [
+        { '--- ELEC': '' },
+        { 'BAT 1, BAT EMER 1': 'ON' },
+        { 'BAT EMER 2, BAT 2': 'ON' },
+        { 'EXT 2': 'ON' },
+        { 'EXT 1': 'ON' },
+        { '--- ADIRS': '' },
+        { 'NAV 1 (L)': 'NAV' },
+        { 'NAV 3 (R)': 'NAV' },
+        { 'NAV 2 (MID)': 'NAV' },
+        { 'NAV Lights': '2' },
+        { 'EMER EXIT LTS': 'ARM' },
+        { 'EIS/CAS': 'CHECK' },
+        { 'IFR CLEARANCE': 'OBTAINED' },
+        { 'OXYGEN Crew Supply': 'ON' },
+        { 'EVAC CAPT & PURS': 'ON' },
+        { '--- FUEL': '' },
+        { 'L TK Main': 'ON' },
+        { 'R TK Main': 'ON' },
+        { 'L TK STBY': 'ON' },
+        { 'R TK STBY': 'ON' },
+        { 'L CTR TK': 'ON' },
+        { 'R CTR TK': 'ON' },
+        { 'Smoking & Phones Lights': 'AUTO' },
+        { 'Exterior/Interior Lights': 'AS REQ' }
+      ]
+    },
+    {
+      title: 'Takeoff PERF',
+      items: [
+        { '--- EFB': '' },
+        { 'FLT OPS STS': 'SET FLIGHT DETAILS' },
+        { 'LOADSHEET': 'SET ZFW & FUEL LOAD' },
+        { '--- FMS': '' },
+        {
+          'ACTIVE/INIT': 'REQUEST OR SET F-PLN',
+          'info': 'ACTIVE/INIT - CPNY F-PLN - Request from Simbrief and Insert'
+        },
+        { '--- FMS (ACTIVE/F-PLN)': '' },
+        { 'DEPARTURE': 'RWY, VIA, STAR INSERTED' },
+        { 'DESTINATION': 'RWY, APPR, VIA, STAR INSERTED' },
+        { 'CONTINUITY': 'CONFIRMED' },
+        {
+          'Altimeter Transition': 'SET to Regional',
+          'info':
+            'Check flight plan Charts Transition Altitude (TA). Change (T.O & APPR): FMS/ACTIVE/PERF - TRANS'
+        },
+        { 'T.O Perf (EFB)': 'SYNC SIMBRIEF, FMS, CONDITIONS' },
+        { 'T.O Speeds': 'COMPUTE & SEND TO FMS' },
+        { '--- FMS (ACTIVE/PERF/T.O)': '' },
+        { 'TakeOff Speeds': 'INSERTED' },
+        { 'FLAPS': 'SET RECOMMENDED' },
+        { 'ICE Protection': 'AS REQ' },
+        { 'ACTIVE/FUEL&LOAD': 'SET FUEL & PAX' },
+        { '--- FMS (SURV/CONTROLS)': '' },
+        { 'WXR': 'AUTO' },
+        { 'TAWS': 'ON' },
+        { 'XPDR': 'SQWK SET, AUTO ON' },
+        { 'TCAS': 'TA/RA & ADS-B ON' }
+      ]
+    },
+    {
       title: 'Before Engine Start',
       items: [
-        { 'Cockpit Prep': 'COMPLETED (BOTH)' },
-        { 'Gear Pins & Covers': 'REMOVED' },
-        { 'Fuel QTY': '___ KG' },
-        { 'T.O Data': 'SET' },
-        { 'Baro Ref': 'SET (BOTH)' },
-        { 'Seat Belts': 'ON' },
-        { 'ADIRS': 'NAV' },
-        { 'Doors': 'CLOSED/ARMED (BOTH)' },
+        { 'START CLEARANCE': 'OBTAINED' },
+        { 'Throttles': 'IDLE' },
+        { 'PARKING BRAKE': 'SET' },
+        { 'CABIN DOOR': 'CLOSED' },
+        { 'ECAM Status': 'CHECK' },
+        { 'Doors': 'CLOSED/ARMED' },
         { 'Thrust Levers': 'IDLE' },
         { 'Park Brk': 'AS REQ' },
-        { 'SURV': 'DEFAULT (BOTH)' },
-        { 'Beacon': 'ON' }
+        { '--- APU': '' },
+        { 'MASTER SW': 'ON' },
+        { 'START': 'ON (Wait for AVAIL)' },
+        { 'APU STARTING': 'MONITOR', 'info': 'PEDESTAL/ECAM/APU' },
+        { '--- AIR': '' },
+        { 'APU BLEED': 'ON' },
+        { 'LIGHTS (SB, Beacon)': 'ON' }
       ]
     },
     {
       title: 'Engine Start',
       items: [
-        { 'Guidance and Considerations': 'REVIEWED' },
-        { 'Engine 1(2)': 'START' },
+        { 'ENG Start Selector': 'IGN START' },
+        { 'ENG 2': 'START' },
+        { 'ECAM Status': 'CHECK' },
+        { 'Engine Instruments': 'MONITOR, AVAIL' },
+        { 'ENG 1': 'START' },
+        { 'ECAM Status': 'CHECK' },
+        { 'Engine Instruments': 'MONITOR, AVAIL' },
         { 'ENG Start Selector': 'NORM' },
-        { 'XBLEED': 'OPEN' },
         { 'APU BLEED': 'OFF' },
-        { 'APU': 'OFF' },
-        { 'ANTIICE Running ENG': 'AS REQ' },
-        { 'ECAM Status': 'CHECK' },
-        { 'TUG & GND CREW': 'DISCONNECT' },
-        { 'ENG Start Selector': 'IGN/START' },
-        { 'ENG 2(1)': 'START' },
-        { 'CHRONO': 'START' },
-        { 'XBLEED': 'AUTO' },
-        { 'ANTI ICE Running ENGS': 'AS REQ' },
-        { 'GND Spoilers': 'ARM' },
-        { 'Rudder Trim Check': 'ZERO' },
-        { 'Flaps': 'SET' },
-        { 'Pitch Trim': 'CHECK' },
-        { 'ECAM Status': 'CHECK' },
-        { 'A-ICE': 'AS REQ' },
-        { 'ECAM STS': 'CHECK' },
-        { 'Rudder Trim': 'NEUTRAL' }
+        { 'APU Master SW': 'OFF' },
+        { 'GND Spoilers': 'ARM', 'info': 'Pull upward the GND SPLRS stick' },
+        { 'Rudder Trim': 'CHECK' },
+        { 'Pitch Trim': 'CHECK', 'info': 'Automatically set' },
+        { 'ECAM STS': 'CHECK' }
+      ]
+    },
+    {
+      title: 'Before Taxi',
+      items: [
+        { 'Flight Plan': 'DEPARTURE VERIFIED' },
+        { 'Altitude': 'SET' },
+        { 'Brakes': 'CHECK & SET & RETRACTED' },
+        { 'Transponder': 'ON & SET' },
+        {
+          'Flight Controls': 'ALL OPERATIONAL',
+          'info': 'ECAM/F-CTL'
+        },
+        { 'FD': 'ON' },
+        { 'AP1': 'ON' },
+        { 'A/THR': 'ON (IF NOT ACTIVE)' },
+        { 'EFIS': 'WX/TERR AS REQ' },
+        { 'BARO': 'SET departure value', 'info': 'From ATIS, EFB or METAR' },
+        { 'Auto Brk': 'ON' },
+        { 'CHOCKS & GPU': 'DISCONNECTED' },
+        { 'T.O Config': 'VERIFY NORMAL', 'info': 'ACTIVE/PERF/T.O' },
+        { 'T.O RWY': 'CONFIRMED' },
+        { 'PACKS': 'OFF' },
+        { 'Taxi Lights': 'ON' }
+      ]
+    },
+    {
+      title: 'Taxi',
+      items: [
+        { 'Taxi Clearance': 'OBTAINED' },
+        { 'Parking Brake': 'RELEASE' },
+        { 'Brakes & Steering': 'CHECK' }
       ]
     },
     {
       title: 'Before Takeoff',
       items: [
-        { 'F/CTL Check': 'COMPLETED (BOTH)' },
-        { 'Flight Instruments': 'CHECKED (BOTH)' },
-        { 'Flaps Setting': 'CONF SET (BOTH)' },
-        { 'V1/VR/V2/THR Rating': '(BOTH)' },
-        { 'Squawk': 'SET' },
-        { 'Seat Belts': 'ON' },
-        { 'GND SPLRS': 'ARM' },
-        { 'Flaps': 'T.O' },
-        { 'Auto Brk': 'RTO' },
-        { 'T.O Config': 'NORMAL' },
-        { 'T.O RWY': 'CONFIRMED (BOTH)' },
-        { 'PACKS': 'OFF' }
+        { 'Runway Clearance': 'OBTAINED' },
+        { 'Flaps': 'CHECK' },
+        { 'Trim': 'CHECK' },
+        { 'Speed Indicators': 'CHECK' },
+        { 'Strobe': 'ON' },
+        { 'Landing Lights': 'ON' },
+        { 'Nose T.O': 'ON' }
+      ]
+    },
+    {
+      title: 'Takeoff',
+      items: [
+        { 'Departure Clearance': 'OBTAINED' },
+        { 'Runway Alignment': 'CONFIRM' },
+        { 'Heading Bug': 'SET' },
+        { 'Throttles': 'FLEX/TOGA' },
+        { 'Positive Rate': 'GEAR UP' },
+        { 'AP': 'ENGAGE, AS REQ' }
       ]
     },
     {
       title: 'Climb',
       items: [
-        { 'LDG Gear': 'UP' },
-        { 'Flaps': '0' },
+        { 'Throttles': 'CLB' },
+        { 'Flaps': 'RETRACT at Safe Speed' },
+        {
+          'GND Spoilers': 'DISARM',
+          'info': 'Pull downward the GND SPLRS stick'
+        },
         { 'PACKS': 'ON' },
-        { 'Baro Ref': 'SET (BOTH)' }
+        { 'Landing Lights': 'OFF' },
+        { 'Nose T.O': 'OFF' },
+        { 'Autopilot': 'MONITOR' },
+        { 'Cruise Altitude': 'SET' },
+        { 'BARO': 'SET to STD' },
+        { 'CLB Performance': 'MONITOR' }
       ]
     },
     {
       title: 'Cruise',
       items: [
         { 'Engine instruments': 'Monitor' },
-        { 'Fuel': 'Balance and quantity checked' },
-        { 'Systems': 'Monitor' }
+        { 'SB Lights': 'OFF' },
+        { 'Fuel Balance': 'MONITOR' },
+        { 'CRZ Performance': 'Monitor' },
+        {
+          'Flight Plan': 'APPROACH VERIFIED / SET',
+          'info': 'Verify all Legs to Runway'
+        },
+        {
+          'LDG PERF (EFB)': 'SYNC SIMBRIEF, FMS, CONDITIONS',
+          'info':
+            'RWY Condition, LDG Technique and BRK Mode may need to be set.'
+        },
+        { 'COMPUTE Distance': 'CHECK, Note DISTANCE' },
+        { '--- FMS (POSITION/NAVAIDS/LS)': '' },
+        {
+          'Approach FREQ/CHAN': 'SET',
+          'info': 'ILS will be auto tuned within 300 NM of destination.'
+        },
+        {
+          'Approach IDENT': 'SET',
+          'info':
+            'From Instrument Approach Chart, identity next to ILS frequency'
+        },
+        { '--- FMS (ACTIVE/PERF/APPR)': '' },
+        { 'WIND, OAT, QNH': 'SET' },
+        { 'MINIMUMS': 'SET' },
+        { 'Approach Speeds': 'CHECK' },
+
+        {
+          'Altitude': 'SET RWY + 2,000 ft',
+          'info': 'Or follow ATC guidance until APPROACH altitude'
+        },
+        {
+          'Altitude Constraints': 'FIX FAF to Signal altitude',
+          'info':
+            'You may set the Final Approach Fix to 20-50 ft below the recommended altitude, to make sure the Signal is captured, in case of inaccurate altimeter setting.'
+        },
+        {
+          'Speed Constraints': 'CHECK in Flight Plan',
+          'info': 'Change approach speeds if necessary'
+        },
+        { 'Auto Brk': 'ON' },
+        {
+          'BTV': 'CALCULATED & SET ',
+          'info':
+            'Get EFB/LDG PERF/Factored LD (M) and set the nearest Runway exit after the landing distance in (PMD/Landing/BTV EXIT AT) - double arrow when PMD Zoom option active.'
+        },
+        { 'TOD POINT': 'CONFIRM' }
       ]
     },
     {
       title: 'Descent',
       items: [
-        { 'ATIS and Weather': 'Obtained and Checked' },
-        { 'FMS/MCDU': 'Set for arrival' },
-        { 'Barometric reference': 'Set to QNH below transition level' },
-        { 'Seat belts': 'ON' }
+        { 'Clearance': 'OBTAINED' },
+        {
+          'MANAGED DESCENT': 'ACTIVATED',
+          'info': 'Once TOD reached, push ALTITUDE knob - verify DES on PMD'
+        },
+        { 'ATIS & Weather': 'CHECK' },
+        {
+          'BARO': 'PRE-SET destination value',
+          'info': 'From ATIS, EFB or METAR'
+        },
+        { 'Instrument Guide (LS)': 'ON' },
+        { 'SB Lights': 'ON' },
+        { 'Landing Lights': 'ON' }
       ]
     },
     {
       title: 'Approach',
       items: [
-        { 'Seat Belts': 'ON' },
-        { 'Minimum': 'SET (BOTH)' },
-        { 'Baro Ref': 'SET (BOTH)' }
+        { 'Clearance': 'OBTAINED' },
+        { 'BARO': 'ARM destination' },
+        { 'Altitude': 'MONITOR' },
+        { 'Flaps': 'AS REQUIRED' },
+        { 'Speed': 'MANAGE/MONITOR' },
+        { 'GND Spoilers': 'ARM', 'info': 'Pull upward the GND SPLRS stick' },
+        { 'Approach': 'APPR (G/S) stby at IAF' },
+        { 'Localizer': 'CONFIRM LOC' },
+        { 'Gear': 'DOWN by FAF' },
+        { 'G/S': 'ARMED at FAF' },
+        { 'Flaps': 'LDG' },
+        {
+          'Trim': 'SET for Landing',
+          'info':
+            'If AP armed let it settle the trim after flap (full) extension'
+        },
+        { 'Auto Brk & SPLRS': 'SET & ARMED' },
+        { 'RWY Cond': 'SET' }
       ]
     },
     {
       title: 'Landing',
       items: [
+        { '--- Manual': '' },
+        { 'WIND': 'CHECK' },
         { 'A/THR': 'SPEED/OFF' },
-        { 'Auto Brk': 'AS REQ' },
-        { 'RWY Cond': 'SET' },
-        { 'Seat Belts': 'ON' },
-        { 'LDG Gear': 'DOWN' },
-        { 'GND SPLRs': 'ARM' },
-        { 'Flaps': 'LDG' }
+        { 'AP': 'DISCONNECT' },
+        { 'Throttles': 'IDLE at 100 ft' },
+        { 'Touchdown': 'FLARE at 40 ft' },
+        { 'Reverse Thrust': 'AS REQ' },
+        { '--- Auto Land': '' },
+        { 'AP1 & AP2': 'ON' },
+        { 'Touchdown': 'FLARE at 40 ft' }
       ]
     },
     {
       title: 'After Landing',
       items: [
         { 'GND SPLRS': 'DISARM' },
-        { 'Flaps': '0' },
-        { 'APU': 'START' },
-        { 'ECAM Status': 'CHECK' },
-        { 'APU': 'AVAIL' },
-        { 'ENG 1(2)': 'SHUTDOWN' }
+        { 'Flaps': 'RETRACT' },
+        { 'Taxi & Parking Clearance': 'OBTAINED' },
+        { 'FD': 'OFF' },
+        { 'Taxi Lights': 'ON' },
+        { 'LDG, SB Lights': 'OFF' },
+        { 'APU MASTER SW': 'ON' },
+        { 'APU START': 'AVAIL' },
+        { 'ENG 1': 'SHUTDOWN' },
+        { 'ECAM Wheel Temps': 'MONITOR' }
       ]
     },
     {
       title: 'Shutdown',
       items: [
-        { 'EXT LT': 'AS REQ' },
-        { 'PARK BRK/CHOCKS': 'AS REQ' },
-        { 'XPDR': 'STBY' },
+        { 'Parking Brake': 'SET' },
         { 'APU BLEED': 'ON' },
-        { 'ALL ENGS': 'OFF' },
-        { 'Fuel Pumps': 'OFF' },
-        { 'Seat Belts': 'OFF' },
-        { 'ADIRS': 'OFF' },
-        { 'Oxygen Crew Supply': 'OFF' },
+        { 'ENG 2': 'SHUTDOWN' },
+        { 'Lights': 'OFF' },
+        { '--- FUEL': '' },
+        { 'R TK STBY': 'OFF' },
+        { 'R TK Main': 'OFF' },
+        { 'L TK STBY': 'OFF' },
+        { 'L TK Main': 'OFF' },
+        { 'R CTR TK': 'OFF' },
+        { 'L CTR TK': 'OFF' },
+        { '--- ADIRS': '' },
+        { 'NAV 1 (L)': 'OFF' },
+        { 'NAV 3 (R)': 'OFF' },
+        { 'NAV 2 (MID)': 'OFF' },
+        { 'Transponder': 'STBY' },
         { 'APU BLEED': 'OFF' },
-        { 'Emer Exit LT': 'OFF' },
-        { 'Signs': 'OFF' },
-        { 'APU': 'AS REQ' },
-        { 'ALL 4 BATS': 'AS REQ' }
+        { 'APU MASTER SW': 'OFF' },
+        { 'OXYGEN Crew Supply': 'OFF' },
+        { 'EVAC CAPT & PURS': 'OFF' },
+        { 'ECAM STATUS': 'CHECK' },
+        { '--- ELEC': '' },
+        { 'EXT 1': 'OFF' },
+        { 'BAT 2, BAT EMER 2': 'OFF' },
+        { 'BAT EMER 1, BAT 1': 'OFF' },
+        { 'EXT 2': 'OFF' }
       ]
     }
   ]
